fd2d51d7786302f8301b6629a94dd1b577bf9c21
becca <rebecca.kaci@gmail.com>
2021-12-02 13:59:48 +0100

    affichage des personnages avec un scroll horizontal

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+ 
+const App = () => {
+  const [characters, setCharacters] = useState([])
 
-export default function App() {
-  return (
+  useEffect(() => {
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then(response => response.json())
+      .then(data => setCharacters(data))
+  }, [] )
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        data={characters} 
+        renderItem={Character} 
+        horizontal 
+      />
     </View>
-  );
+  )
+}
+
+const Character = ({ item }) => {
+  return(
+    <View style={styles.imgContainer}>
+      <Image 
+        style={styles.image}
+        source={{uri: item.imageUrl}}
+      />
+      <Text>{item.fullName}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imgContainer: {
+    margin: 40,
+  },
+  image: {
+    width: 200,
+    height: 300,
+  }
 });
+
+export default App
